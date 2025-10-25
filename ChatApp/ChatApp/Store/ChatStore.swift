c46769f40ff5a580bcf8489ffeed2c97d03d5dc7
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2025-10-25 17:06:10 +0100

    create methods for chat event

--- ChatApp/ChatApp/Store/ChatStore.swift
+++ ChatApp/ChatApp/Store/ChatStore.swift
@@ -14,7 +14,7 @@ final class ChatStore: ObservableObject {
     
     @Published private(set) var events: OrderedDictionary<String, ChatStoreEvent> = [:]
     
-    func reconcileEventDTO(eventDto: ChatEventDTO) {
+    func reconcileChatEventDTO(eventDto: ChatEventDTO) {
         if var existingEvent = events[eventDto.id] {
             // Update existing event from event DTO
             eventDto.updateDomain(&existingEvent)
@@ -26,6 +26,15 @@ final class ChatStore: ObservableObject {
         }
 
         resort()
+    }
+    
+    func addChatEvent(event: ChatStoreEvent) {
+        events[event.id] = event
+    }
+    
+    func updateChatEvent(id: String, updateFn: (inout ChatStoreEvent) -> Void) {
+        guard let index = events.index(forKey: id) else { return }
+        updateFn(&events.values[index])
     }
     
     private func resort() {
