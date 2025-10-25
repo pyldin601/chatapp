0002c1d5ab78f354daf700630d680e5b53738fc8
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2025-10-25 16:27:08 +0100

    rework store with OrderedDictionary

--- ChatApp/ChatApp/Store/ChatStore.swift
+++ ChatApp/ChatApp/Store/ChatStore.swift
@@ -7,39 +7,35 @@
 
 import SwiftUI
 import Combine
+import OrderedCollections
 
 @MainActor
 final class ChatStore: ObservableObject {
     
-    @Published private(set) var events: [ChatStoreEvent] = []
+    @Published private(set) var events: OrderedDictionary<String, ChatStoreEvent> = [:]
     
     func reconcileEventDTO(eventDto: ChatEventDTO) {
-        
-        if var existingEvent = events.first(where: { $0.id == eventDto.id }) {
+        if var existingEvent = events[eventDto.id] {
             // Update existing event from event DTO
             eventDto.updateDomain(&existingEvent)
         } else {
             // Create new event from event DTO
             let event = eventDto.toDomain()
             
-            events.append(event)
+            events[event.id] = event
         }
-        
+
         resort()
-        
     }
     
     private func resort() {
-        
         events.sort { lhs, rhs in
-            switch (lhs.sequence, rhs.sequence) {
+            switch (lhs.value.sequence, rhs.value.sequence) {
             case let (x?, y?): return x < y
-            case (nil, nil):   return lhs.createdAt < rhs.createdAt
+            case (nil, nil):   return lhs.value.createdAt < rhs.value.createdAt
             case (nil, _):     return false
             case (_, nil):     return true
             }
         }
-        
     }
-    
 }
