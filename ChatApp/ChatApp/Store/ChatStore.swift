87acfc42076ca1f08400942b26026850aa808d53
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2025-10-30 22:40:48 +0000

    do not resort events in the chat

--- ChatApp/ChatApp/Store/ChatStore.swift
+++ ChatApp/ChatApp/Store/ChatStore.swift
@@ -18,8 +18,6 @@ final class ChatStore {
         } else {
             events[eventDto.id] = eventDto.toDomain(ownNickname)
         }
-
-        resort()
     }
 
     func addChatEvent(event: ChatStoreEvent) {
@@ -29,7 +27,6 @@ final class ChatStore {
     func updateChatEvent(id: String, updateFn: (inout ChatStoreEvent) -> Void) {
         guard let index = events.index(forKey: id) else { return }
         updateFn(&events.values[index])
-        resort()
     }
 
     private func resort() {
